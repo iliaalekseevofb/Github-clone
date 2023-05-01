ed2a6aa71d173da91da8796da7180bcf0657ad2b
iliaalekseevofb <iliaalekseevofb@gmail.com>
2023-05-01 21:37:31 +0500

    Browser router basename is changed to correct

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import App from './App'
@@ -9,7 +9,7 @@ import './App.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="https://iliaalekseevofb.github.io/Github-clone/">
+      <BrowserRouter basename="Github-clone">
         <App />
       </BrowserRouter>
     </Provider>
